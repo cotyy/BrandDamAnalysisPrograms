ecead0972c9a523e29dba3f8a4b19a0ccd58cb68
cotyy <76697073+cotyy@users.noreply.github.com>
2025-04-08 17:47:53 -0400

    Update bmpwrt() and sentance() first arg.

--- common/brandgraph19.h
+++ common/brandgraph19.h
@@ -17,7 +17,8 @@ struct vctr2
 
 
 float magvec(vctr2 p1, vctr2 p2);
-void bmpwrt(char fnm[]);
+//void bmpwrt(char fnm[]);
+void bmpwrt(const char* fnm);
 void rctngl(int px1, int py1, int px2, int py2, int clr);
 void palette();
 void fatline(vctr2 pnt1, vctr2 pnt2, float thc, int clr);
@@ -34,4 +35,5 @@ vctr2 scrnpnt(vctr2 gpnt, vctr2 mdlcntr, float dmscl);
 //font writer
 vctr2 stkfnt(char ltr, float size, float x, float y, float thc, int clr);
 
-void sentance(char word[], float x, float y, float size, float thc, int clr);
+//void sentance(char word[], float x, float y, float size, float thc, int clr);
+void sentance(const char* word, float x, float y, float size, float thc, int clr);
